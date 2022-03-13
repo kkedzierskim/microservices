8977b7df1c16bb2399f9c337aecf3cc5b0c10aa2
Mateusz Kędzierski <mateusz.kedzierski@execon.pl>
2022-03-13 10:29:28 +0100

    rest template between microservices

--- fraud/src/main/java/com/amigoscode/fraud/web/controller/FraudController.java
+++ fraud/src/main/java/com/amigoscode/fraud/web/controller/FraudController.java
@@ -6,9 +6,11 @@ import com.amigoscode.fraud.web.service.FraudCheckService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
 public class FraudController {
 
